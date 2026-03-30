a0b615f68f892183a2ed4c0612151602eb77d726
Kaj Suaning <ksuaning@gmail.com>
2026-03-31 00:39:04 +1100

    Code Cleanup
    Split implementation up into more readable functions.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+/**
+ * Using this project to learn Rust.
+ *
+ */
+
 #[derive(Debug, FromPrimitive, PartialEq)] // Lets us print stuff and copy using .clone()
 #[repr(u8)]
 pub enum PacketType {
@@ -16,17 +21,6 @@ pub enum PacketType {
     PingResp = 13,
     Disconnect = 14,
     Auth = 15,
-}
-
-fn get_packet_type(buffer: &[u8]) -> Option<PacketType> {
-    PacketType::from_u8(buffer[0] >> 4)
-}
-
-#[derive(Debug)]
-pub struct FixedHeader {
-    pub packet_type: PacketType,
-    pub flags: u8,
-    pub remaining_length: usize,
 }
 
 use num_derive::FromPrimitive;
@@ -74,6 +68,51 @@ fn extract_remaining_length(buffer: &[u8]) -> Result<(u32, usize), &'static str>
     Err("My Error")
 }
 
+fn handle_connect(stream: &mut TcpStream){
+    let connack = [0x20, 0x02, 0x00, 0x00]; 
+    // Byte 0: packet type (2) and flags
+    // Byte 1: Remaining Length  2 bytes (0x02)
+    // Byte 2: Ack Flags (Not implemeted)
+    // Byte 3: Return Code (0 for success)
+    match stream.write_all(&connack) {
+        Ok(()) => println!("Sent ConnAck"),
+        Err(e) => eprintln!("Failed sending connack: {}", e),
+    };
+
+}
+
+
+fn handle_publish(buffer: &[u8]){
+    // The payload size is variable len to support large payloads so we need to
+    // loop through it to find where data starts and payload size ends.
+    let (payload_size, start_index) = match extract_remaining_length(&buffer){
+        Ok((payload_size, start_index)) => (payload_size, start_index),
+        Err(e) => {
+            eprintln!("Invalid Packet: {}", e);
+            return;
+        }
+    };
+    let mut current_pos = start_index;
+    // Topci name len maxes out a 2 bytes (not variable like payload)
+    let topic_len = ((buffer[current_pos] as usize) << 8) | (buffer[current_pos + 1] as usize);
+    current_pos += 2;
+    let topic_name = &buffer[current_pos..current_pos + topic_len];
+    match std::str::from_utf8(topic_name){
+        Ok(text) => println!("Topic: {}", text),
+        Err(e) => eprintln!("Failed to decode topic {}", e),
+    }
+    current_pos += topic_len;
+    let qos = (buffer[0] & 0x06) >> 1;
+    if qos > 0 {
+        current_pos += 2; // Skip the 2-byte Packet ID
+    }
+    let payload_bytes = &buffer[current_pos..];
+    match std::str::from_utf8(payload_bytes) {
+        Ok(text) => println!("Payload: {}", text),
+        Err(e) => println!("Payload is binary or invalid UTF-8: {}", e),
+    }
+
+}
 
 fn handle_client(stream: &mut TcpStream) {
     println!("Handling Client");
@@ -83,54 +122,19 @@ fn handle_client(stream: &mut TcpStream) {
         match stream.read(&mut buffer) {
             Ok(n) if n > 0 => {
                 println!("Recieved {} bytes", n);
-                if let Some(packet_type) = PacketType::from_u8(buffer[0] >> 4) {
-                    println!("Packet Type: {:?}", packet_type);
-                    if packet_type == PacketType::Connect{
-                        let connack = [0x20, 0x02, 0x00, 0x00]; 
-                        // Byte 0: packet type (2) and flags
-                        // Byte 1: Remaining Length  2 bytes (0x02)
-                        // Byte 2: Ack Flags (Not implemeted)
-                        // Byte 3: Return Code (0 for success)
-                        match stream.write_all(&connack) {
-                            Ok(()) => println!("Sent ConnAck"),
-                            Err(e) => eprintln!("Failed sending connack: {}", e),
-                        };
-                    }else if packet_type == PacketType::Publish{
-                        // The payload size is variable len to support large payloads so we need to
-                        // loop through it to find where data starts and payload size ends.
-                        let (payload_size, start_index) = match extract_remaining_length(&buffer){
-                            Ok((payload_size, start_index)) => (payload_size, start_index),
-                            Err(e) => {
-                                eprintln!("Invalid Packet: {}", e);
-                                break;
-                            }
-                        };
-                        let mut current_pos = start_index;
-                        // Topci name len maxes out a 2 bytes (not variable like payload)
-                        let topic_len = ((buffer[current_pos] as usize) << 8) | (buffer[current_pos + 1] as usize);
-                        current_pos += 2;
-                        let topic_name = &buffer[current_pos..current_pos + topic_len];
-                        match std::str::from_utf8(topic_name){
-                            Ok(text) => println!("Topic: {}", text),
-                            Err(e) => eprintln!("Failed to decode topic {}", e),
-                        }
-                        current_pos += topic_len;
-                        let qos = (buffer[0] & 0x06) >> 1;
-                        if qos > 0 {
-                            current_pos += 2; // Skip the 2-byte Packet ID
-                        }
-                        let payload_bytes = &buffer[current_pos..];
-                        match std::str::from_utf8(payload_bytes) {
-                            Ok(text) => println!("Payload: {}", text),
-                            Err(e) => println!("Payload is binary or invalid UTF-8: {}", e),
-                        }
+                let packet_type = PacketType::from_u8(buffer[0] >> 4);
+                if let Some(packet_type) = packet_type {
+                    match packet_type {
+                        PacketType::Connect => handle_connect(stream),
+                        PacketType::Publish => handle_publish(&buffer),
+                        _ => println!("Unknown packet type"),
                     }
-                } else{
-                    println!("Unknown pack type");
+                } else {
+                    println!("Unexpected packet type!");
                 }
-                // println!("Buffer: {:?}", buffer);
             }
-            Ok(_) => println!("Client disconected."),
+            Ok(n) if n == 0 => println!("Client disconected."),
+            Ok(_) => {}
             Err(e) => eprintln!("Error Reading from sock: {}", e),
         }
     }
